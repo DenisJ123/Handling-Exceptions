f0909e4ab60695febcd9baf58e6691b6dfa6a856
Denis Johnson <G00403126@atu.ie>
2022-11-07 10:27:44 +0000

    finish

--- src/main/java/Exercise.java
+++ src/main/java/Exercise.java
@@ -1,28 +1,25 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-
-
 public class Exercise {
     public static void main(String[] args){
-        try{
-            System.out.println("Please enter a number between 0 - 9: ");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
+        int Num = 1;
+        while (Num > 0) {
+            try {
+                System.out.println("Please enter a number between 0 - 9: ");
+                Scanner myscan = new Scanner(System.in);
+                int myNum = myscan.nextInt();
 
-            if(myNum <= 9 && myNum >0) {
-                System.out.println("You entered  " + myNum);
-            }
-            else{
-                System.out.println("Not a valid number");
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered  " + myNum);
+                    Num--;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("Invalid");
             }
         }
-        catch(InputMismatchException f)
-        {
-            System.out.println("Invalid");
-        }
-
     }
 }
 
